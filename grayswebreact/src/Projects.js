91f3a27772125de87ede3e7900cd1ab8bfc79238
Graham <grahamfalconer@icloud.com>
2020-09-03 22:44:41 +0100

    fixed iframe content size

--- grayswebreact/src/Projects.js
+++ grayswebreact/src/Projects.js
@@ -4,11 +4,13 @@ export default class Projects extends Component {
   render() {
     return (
       <div>
-        <h2> Graham's Haunted Piano </h2>
+        <h2> Graham's Haunted Piano (Javascript, HTML, CSS) </h2>
         <iframe className="piano" src="https://grahamshauntedpiano.surge.sh/"></iframe>
-        <h2> Honda The Other Side Remake </h2>
+        <h2> Mission-ctrl  (MongoDB, Express, React.js, Node.js) </h2>
+        <iframe className="mission-ctrl" src="https://mission-ctrl.surge.sh/"></iframe>
+        <h2> Honda The Other Side Remake (Javascript, HTML, CSS) </h2>
         <iframe className="honda" src="http://honda-theotherside.surge.sh/" scrolling="no"></iframe>
-        <h2> Cant Touch This </h2>
+        <h2> Cant Touch This  (Javascript, HTML, CSS) </h2>
         <iframe className="ctt-frame" src="http://canttouchthis.surge.sh/" ></iframe>
         
       </div>
