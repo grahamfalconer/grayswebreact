e99e5285b09cce83ae6a20989d6e4166a2a30e94
Graham <grahamfalconer@icloud.com>
2020-09-04 13:17:36 +0100

    changed projects to take up 50% width, 2 per row

--- grayswebreact/src/CVcomponent.js
+++ grayswebreact/src/CVcomponent.js
@@ -4,19 +4,35 @@ export default class CVcomponent extends Component {
   constructor(){
     super();
     this.state = {
-
+      showingPDF: false,
     }
+  }
+  toggleShowCv = () => {
+    this.state.showingPDF ? this.setState({ showingPDF: false }) : this.setState({ showingPDF: true })
   }
 
   
   render() {
-    let showCvButton = <a id="cv-button"> Show CV </a>
-    let downloadCvButton = <a id="downloadCvButton"> Download CV </a>
+    let downloadCvButton = <a href={process.env.PUBLIC_URL + '/CV.pdf'} download id="downloadCvButton"> Download CV </a>
 
-    return (
-      <div>
-        <iframe className="cv-frame" src={process.env.PUBLIC_URL + '/CV.pdf'}></iframe>
-      </div>
-    )
+    if(!this.state.showingPDF){
+      return (
+        <div>
+          <a onClick={this.toggleShowCv} id="cv-button"> Show CV </a> {downloadCvButton}
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          <div>
+          <a onClick={this.toggleShowCv} id="cv-button"> Hide CV </a>  {downloadCvButton}
+          </div>
+          <div>
+            <iframe className="cv-frame" src={process.env.PUBLIC_URL + '/CV.pdf'}></iframe>
+          </div>
+        </div>
+      )
+    }
   }
 }
--- grayswebreact/src/Projects.js
+++ grayswebreact/src/Projects.js
@@ -4,15 +4,40 @@ export default class Projects extends Component {
   render() {
     return (
       <div>
-        <h2> Graham's Haunted Piano (Javascript, HTML, CSS) </h2>
-        <iframe className="piano" src="https://grahamshauntedpiano.surge.sh/"></iframe>
-        <h2> Mission-ctrl  (MongoDB, Express, React.js, Node.js) </h2>
-        <iframe className="mission-ctrl" src="https://mission-ctrl.surge.sh/"></iframe>
-        <h2> Honda The Other Side Remake (Javascript, HTML, CSS) </h2>
-        <iframe className="honda" src="http://honda-theotherside.surge.sh/" scrolling="no"></iframe>
-        <h2> Cant Touch This  (Javascript, HTML, CSS) </h2>
-        <iframe className="ctt-frame" src="http://canttouchthis.surge.sh/" ></iframe>
-        
+        <div class="mission-holder">
+            <h2> Mission-ctrl  (MongoDB, Express, React.js, Node.js) </h2>
+            <iframe className="mission-ctrl" src="https://mission-ctrl.surge.sh/"></iframe>
+            <p> • A game built with JS event listeners to help developers learn VScode shortcuts </p>
+            <p> • Enter the shortcuts as they come up on the screen to earn points  </p>
+            <p> • Submit your score at the end to our leaderboard  </p>
+            <div class="blank-line"> </div>
+            <p> Github repo available <a>here</a> </p>
+        </div>
+        <div class="project-holder">
+            <h2> Cant Touch This  (Javascript, HTML, CSS) </h2>
+            <iframe className="ctt-frame" src="http://canttouchthis.surge.sh/" ></iframe>
+            <p> • McHammer was right when he said "you can't touch this"</p>
+            <p> • Just press play and see how many times you can tap on McHammer in one minute! </p>
+            <div class="blank-line"> </div>
+            <p> Github repo available <a> here </a> </p>
+        </div>
+        <div class="project-holder">
+            <h2> Honda The Other Side Remake (Javascript, HTML, CSS) </h2>
+            <iframe className="honda" src="http://honda-theotherside.surge.sh/" scrolling="no"></iframe>
+            <p> • In this project I recreated the custom video player from the 2015 Honda Civic R 'other side' commercial </p>
+            <p> • You can test the video player by pressing play and clicking the button in the bottom right (or tapping r on your keyboard) </p>
+            <div class="blank-line"> </div>
+            <p> Github repo available <a> Here </a> </p>
+        </div>
+        <div class="project-holder">
+            <h2> Graham's Haunted Piano (Javascript, HTML, CSS) </h2>
+            <iframe className="piano" src="https://grahamshauntedpiano.surge.sh/"></iframe>
+            <p> • A Piano that you can play on your keyboard using keys -> asdfghjkl;'\enter </p>
+            <p> • Don't feel like playing? let the piano do the work for you by pressing on one of the greatest hits</p>
+            <div class="blank-line"> </div>
+            <p> Github repo available <a> here </a></p>
+        </div>
+
       </div>
     )
   }
