7e770dd4ec0d9547b105e1cae622d88262d3326d
Graham <grahamfalconer@icloud.com>
2020-09-09 11:48:56 +0100

    changed project styling

--- grayswebreact/src/Projects.js
+++ grayswebreact/src/Projects.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import bankTechTest from './bank_tech_test.png';
 import gapagBook from './gapagbook.png';
 import missionCtrl from './mission-ctrl.png';
+import honda from './hondaotherside.png'
+import piano from './grahmpiano.png'
+
+
 
 export default class Projects extends Component {
   constructor(){
@@ -15,8 +19,8 @@ export default class Projects extends Component {
             <h2> Mission-ctrl  (MongoDB, Express, React.js, Node.js) </h2>
             <img className="project-image" src={missionCtrl}></img>
             <p> A game built to help developers learn VScode shortcuts </p>
-            <p> • Enter the shortcuts on your keyboard as they come up on the screen to earn points  </p>
-            <p> • Submit your score at the end to our leaderboard  </p>
+            <p>  Enter the shortcuts on your keyboard as they come up on the screen to earn points  </p>
+            <p>  Submit your score at the end to our leaderboard  </p>
             <div class="blank-line"> </div>
             <p> Github repo available <a href="https://github.com/grahamfalconer/mission-ctrl">here</a> </p>
             <p> Live version hosted <a href="http://mission-ctrl.surge.sh/">here</a> </p>
@@ -26,7 +30,7 @@ export default class Projects extends Component {
             <h2> Bank Tech Test (Ruby) </h2>
             <img className="project-image" src={bankTechTest}></img>
             <p>  A recreation of a Bank Account with features such as withdraw, deposit and balance.</p>
-            <p> • Instructions on how to use it can be found at the projects README on github </p>
+            <p>  Instructions on how to use it can be found at the projects README on github </p>
             <div class="blank-line"> </div>
             <p> Github repo available <a href="https://github.com/grahamfalconer/Bank_tech_task"> here </a> </p>
         </div>
@@ -41,29 +45,29 @@ export default class Projects extends Component {
         <div class="blank-line"></div>
         <div class="project-holder">
             <h2> Honda The Other Side Remake (Javascript, HTML, CSS) </h2>
-            <iframe className="honda" src="http://honda-theotherside.surge.sh/" scrolling="no"></iframe>
+            <img className="project-image" src={honda}></img>
             <p> In this project I recreated the custom video player from the 2015 Honda Civic R 'other side' commercial </p>
-            <p> • You can test the video player by pressing play and clicking the button in the bottom right (or tapping r on your keyboard) </p>
+            <p> You can test the video player by pressing play and clicking the button in the bottom right (or tapping r on your keyboard) </p>
             <div class="blank-line"> </div>
             <p> Github repo available <a href="https://github.com/grahamfalconer/hondaR-otherside"> Here </a> </p>
             <p> Live version hosted <a href="http://honda-theotherside.surge.sh/">here</a> </p>
         </div>
         <div class="project-holder">
             <h2> Graham's Haunted Piano (Javascript, HTML, CSS) </h2>
-            <iframe className="piano" src="https://grahamshauntedpiano.surge.sh/"></iframe>
+            <img className="project-image" src={piano}></img>
             <p>  A Piano that you can play on your keyboard  </p>
             <p>  use keys -> asdfghjkl;'\enter   </p>
-            <p> • Don't feel like playing? let the piano do the work for you by pressing on one of the greatest hits</p>
+            <p> Don't feel like playing? let the piano do the work for you by pressing on one of the greatest hits</p>
             <div class="blank-line"> </div>
             <p> Github repo available <a href="https://github.com/grahamfalconer/Haunted-Piano"> here </a></p>
             <p> Live version hosted <a href="https://grahamshauntedpiano.surge.sh/">here</a> </p>
         </div>
         <div class="blank-line"></div>
-        <div class="project-holder">
+        <div class="mission-holder">
           <h2> My Portfolio (React.js, CSS) </h2>
           <iframe className="ctt-frame" src="http://grahamfalconer.surge.sh/"></iframe>
           <p>   Weird seeing this project live loaded on itself, right ? It gave me Interstellar vibes </p>
-          <p> • This project allows you to download and view a PDF of my CV, view my side projects as well as gives you the option to contact me  </p>
+          <p>   This project allows you to download and view a PDF of my CV, view my side projects as well as gives you the option to contact me  </p>
           <div class="blank-line"> </div>
           <p> Github repo available <a href="https://github.com/grahamfalconer/grayswebreact"> here </a></p>
           <p> You are currently on the live version </p>
